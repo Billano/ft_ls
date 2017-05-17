31a04c861015416504864faee0c58d86d098dfd3
eugenio <eugenio.606@gmail.com>
2017-05-17 05:54:06 -0700

    l format working, but missing the total part and to create the time extractors

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: eurodrig <eurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 03:13:47 by eurodrig          #+#    #+#             */
-/*   Updated: 2017/05/16 22:18:07 by eurodrig         ###   ########.fr       */
+/*   Updated: 2017/05/17 05:42:32 by eurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ft_avl_height(t_avl_tree_ls *node)
 	return (node->height);
 }
 
-t_avl_tree_ls *ft_avl_tree_ls_create(struct dirent *f_dir, char *path)
+t_avl_tree_ls *ft_avl_tree_ls_create(struct dirent *f_dir, struct stat f_stat, char *path)
 {
 	t_avl_tree_ls *node;
 	char *n_path;
@@ -29,9 +29,10 @@ t_avl_tree_ls *ft_avl_tree_ls_create(struct dirent *f_dir, char *path)
 		return (0);
 	n_path =  ft_strjoin(ft_strjoin(path, "/"), f_dir->d_name);
 	node->path = n_path;
-	lstat(n_path, &(node->f_stat));
+	// lstat(n_path, &(node->f_stat));
+	node->f_stat = f_stat;
 	node->d_name = ft_strdup(f_dir->d_name);
-	node->d_namlen = f_dir->d_namlen;
+	node->size = (long long)f_stat.st_size;
 	node->left = NULL;
 	node->right = NULL;
 	node->height = 1;
@@ -51,7 +52,7 @@ void ft_avl_tree_ls_inorder(t_avl_tree_ls *root, char *path, t_ls_flags flags)
 	if (root)
 	{
 		ft_avl_tree_ls_inorder(root->left, path, flags);
-		flags.l_flag? printf("long: |%s|\n", root->d_name) : printf("%s\n", root->d_name);
+		flags.l_flag ? ft_ls_print_long(root) : printf("%s\n", root->d_name);
 		ft_avl_tree_ls_inorder(root->right, path, flags);
 	}
 }
@@ -61,7 +62,7 @@ void ft_avl_tree_ls_backorder(t_avl_tree_ls *root, char *path, t_ls_flags flags)
 	if (root)
 	{
 		ft_avl_tree_ls_backorder(root->right, path, flags);
-		flags.l_flag? printf("long: |%s|\n", root->d_name) : printf("%s\n", root->d_name);
+		flags.l_flag ? ft_ls_print_long(root) : printf("%s\n", root->d_name);
 		ft_avl_tree_ls_backorder(root->left, path, flags);
 	}
 }
@@ -94,14 +95,20 @@ void ft_avl_tree_ls_r_backorder(t_avl_tree_ls *root, char *path, t_ls_flags flag
 	}
 }
 
-t_avl_tree_ls *ft_avl_tree_ls_insert(t_avl_tree_ls *root, struct dirent *f_dir, char *path)
+t_avl_tree_ls *ft_avl_tree_ls_insert(t_avl_tree_ls *root, struct dirent *f_dir, struct stat f_stat, char *path)
 {
 	if (!root)
-		return (ft_avl_tree_ls_create(f_dir, path));
+		return (ft_avl_tree_ls_create(f_dir, f_stat, path));
 	else if (ft_strcmp(root->d_name, f_dir->d_name) >= 0)
-		root->left = ft_avl_tree_ls_insert(root->left, f_dir, path);
+	{
+		root->size += (long long)f_stat.st_size;
+		root->left = ft_avl_tree_ls_insert(root->left, f_dir, f_stat, path);
+	}
 	else
-		root->right = ft_avl_tree_ls_insert(root->right, f_dir, path);
+	{
+		root->size += (long long)f_stat.st_size;
+		root->right = ft_avl_tree_ls_insert(root->right, f_dir,f_stat,  path);
+	}
 	return (root);
 }
 
@@ -109,6 +116,7 @@ t_avl_tree_ls *ft_store_dir(DIR *fd, t_ls_flags flags, char *path)
 {
 	t_avl_tree_ls *root;
 	struct dirent *f_dir;
+	struct stat f_stat;
 
 	f_dir = 0;
 	root = 0;
@@ -117,7 +125,10 @@ t_avl_tree_ls *ft_store_dir(DIR *fd, t_ls_flags flags, char *path)
 	while ((f_dir = readdir(fd)))
 	{
 		if (flags.a_flag || f_dir->d_name[0] != '.')
-			root = flags.t_flag ? ft_avl_tree_ls_insert(root, f_dir, path) : ft_avl_tree_ls_insert(root, f_dir, path);//aqui se guarda por tiempo
+		{
+			lstat(ft_strjoin(ft_strjoin(path, "/"), f_dir->d_name), &f_stat);
+			root = flags.t_flag ? ft_avl_tree_ls_insert(root, f_dir, f_stat, path) : ft_avl_tree_ls_insert(root, f_dir, f_stat, path);//aqui se guarda por tiempo
+		}
 	}
 	return (root);
 }
@@ -164,17 +175,67 @@ void ft_open_dir(char *path, t_ls_flags flags)
 		return ;
 	fd = opendir(path);
 	root = ft_store_dir(fd, flags, path);
+	if (flags.l_flag)
+		printf("total %lld\n", root->size);
 	flags.r_flag ? ft_avl_tree_ls_backorder(root, path, flags) : ft_avl_tree_ls_inorder(root, path, flags);
 	if (flags.bigr_flag)
 	{
+		// printf("total %lld\n", root->size);
 		flags.r_flag ? ft_avl_tree_ls_r_backorder(root, path, flags) : ft_avl_tree_ls_r_inorder(root, path, flags);
 	}
 	closedir(fd);
 }
 
-void ft_ls_print_long(t_avl_tree_ls *root, char *path)
+char ft_file_type(t_avl_tree_ls *root)
 {
+	if (S_ISDIR(root->f_stat.st_mode))
+		return ('d');
+	else if (S_ISREG(root->f_stat.st_mode))
+		return ('-');
+	else if (S_ISLNK(root->f_stat.st_mode))
+		return ('l');
+	else if (S_ISCHR(root->f_stat.st_mode))
+		return ('c');
+	else if (S_ISBLK(root->f_stat.st_mode))
+		return ('b');
+	else if (S_ISFIFO(root->f_stat.st_mode))
+		return ('p');
+	else if (S_ISSOCK(root->f_stat.st_mode))
+		return ('s');
+	else
+		return ('.');
+}
 
+char *ft_file_permisions(t_avl_tree_ls *root)
+{
+	char *str;
+
+	str = ft_strnew(11);
+	str[0] = ft_file_type(root);
+	str[1] = (root->f_stat.st_mode & S_IRUSR) ? 'r' : '-';
+	str[2] = (root->f_stat.st_mode & S_IWUSR) ? 'w' : '-';
+	str[3] = (root->f_stat.st_mode & S_IXUSR) ? 'x' : '-';
+	str[4] = (root->f_stat.st_mode & S_IRGRP) ? 'r' : '-';
+	str[5] = (root->f_stat.st_mode & S_IWGRP) ? 'w' : '-';
+	str[6] = (root->f_stat.st_mode & S_IXGRP) ? 'x' : '-';
+	str[7] = (root->f_stat.st_mode & S_IROTH) ? 'r' : '-';
+	str[8] = (root->f_stat.st_mode & S_IWOTH) ? 'w' : '-';
+	str[9] = (root->f_stat.st_mode & S_IXOTH) ? 'x' : '-';
+	return (str);
+}
+
+
+
+void ft_ls_print_long(t_avl_tree_ls *root)
+{
+	struct passwd *pwd;
+	struct group *group;
+
+	pwd = 0;
+	pwd = getpwuid(root->f_stat.st_uid);
+	group = 0;
+	group = getgrgid(root->f_stat.st_gid);
+	printf("%s	%ld	%s	%s	%lld	%.12s	%s\n", ft_file_permisions(root), (long)root->f_stat.st_nlink, pwd->pw_name, group->gr_name, (long long)root->f_stat.st_size, ctime(&(root->f_stat.st_mtime)) + 4, root->d_name);
 }
 
 t_ls_flags ft_ls_flag_parser(char *str)
